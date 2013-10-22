b45cf78de5c86cc28b242729fb9cc967e19be7fc
Ivan Santos <ivan@quimbik.com>
2013-10-22 11:34:39 -0500

    Tested Workflow

---
+++ Vagrantfile
@@ -0,0 +1,50 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "precise32"
+
+
+  # The url from where the 'config.vm.box' box will be fetched 
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+
+
+  # accessing "localhost:3000" will access port 3000 on the guest machine.
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+
+  # using a specific IP.
+  # config.vm.network :private_network, ip: "10.4.4.57"
+
+
+  # If true, then any SSH connections made will enable agent forwarding.
+  # config.ssh.forward_agent = true
+
+
+  # Share an additional folder to the guest VM. The first argument is
+  # the path on the host to the actual folder. The second argument is
+  # the path on the guest to mount the folder.
+  # config.vm.synced_folder '------YourSharedFolderDirectory-----', '/home/vagrant/code', :nfs=> false
+
+
+  # Provider-specific configuration so you can fine-tune various
+  # backing providers for Vagrant. These expose provider-specific options.
+  # config.vm.provider :virtualbox do |v|
+  #   v.customize ["modifyvm", :id, "--memory", "4000"]
+  #   v.customize ["modifyvm", :id, "--cpuexecutioncap", "95"]
+  #   v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+  #   v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+  # end
+
+
+  #Provision
+  config.vm.provision :puppet,
+    :manifests_path => 'puppet/manifests',
+    :module_path    => 'puppet/modules'
+
+end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,106 @@
+# --- Preinstall Stage ---------------------------------------------------------
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- MySQL --------------------------------------------------------------------
+
+class install_mysql {
+  include '::mysql::server'
+}
+class { 'install_mysql': }
+
+
+
+# --- PostgreSQL ---------------------------------------------------------------
+
+class install_postgres {
+
+  class { 'postgresql::server': }
+
+  postgresql::server::db { 'database_example':
+    user     => 'rails',
+    password => postgresql_password('rails', 'rails'),
+  }
+}
+class { 'install_postgres': }
+
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+
+# --- MongoDB -------------------------------------------------------------------
+class { 'mongodb':
+  init => 'sysv',
+}
+
+
+# --- RMagick system dependencies -------------------------------------------------------------------
+
+package { ['libmagickwand4', 'libmagickwand-dev']:
+ensure => installed,
+}
+
+
+# --- RVM ---------------------------------------------------------------
+
+class install-rvm {
+  include rvm
+  rvm::system_user { vagrant: ; }
+
+  rvm_system_ruby {
+    'ruby-1.9.3-p194':
+      ensure => 'present',
+      default_use => true;
+    'ree-1.8.7-2012.02':
+      ensure => 'present',
+      default_use => false;
+    'ruby-2.0.0-p247':
+      ensure => 'present',
+      default_use => false;
+  }
+
+  rvm_gem {
+    'ruby-1.9.3-p194/bundler': ensure => latest;
+    'ruby-1.9.3-p194/rails': ensure => latest;
+    'ruby-1.9.3-p194/rake': ensure => latest;
+  }
+
+}
+
+class { 'install-rvm': }
+
+
+
+# --- Packages -------------------------------------------------------------------
+
+
+
+package { 'libreadline-dev':
+  ensure => installed
+}
+
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
